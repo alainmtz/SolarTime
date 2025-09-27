5cb1a3cadd36a7be06a08df9f94ff00792943b64
alainmtz <melvinalvin.bello@gmail.com>
2025-09-26 16:00:44 -0900

    refactor: remove unused input for solar cable and update image source in SolarSVG to use relative path

--- src/components/DisenadorFotovoltaico.jsx
+++ src/components/DisenadorFotovoltaico.jsx
@@ -42,7 +42,6 @@ export default function DisenadorFotovoltaico() {
         }
         fetchProductos();
     }, []);
-    // Cable solar: ya no se usa input independiente, solo desde protecciones
 
     // Cargar consumos guardados
     useEffect(() => {
